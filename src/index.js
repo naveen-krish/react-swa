612b51244fb7d26dcd17ca1c6557695fb2bca263
Naveen <naveenkrishiv@gmail.com>
2021-08-09 17:02:52 +0530

    unused import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import Counters from "./components/counters";
+import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
